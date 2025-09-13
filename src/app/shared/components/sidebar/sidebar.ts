b4fb5ad7f3088735f44282a3246eb7670d3e1a68
devmork <enoymarkden@gmail.com>
2025-09-13 21:15:02 +0800

    Add sidebar component

---
+++ src/app/shared/components/sidebar/sidebar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sidebar-component',
+  imports: [],
+  templateUrl: './sidebar.html',
+  styleUrl: './sidebar.css',
+})
+export class Sidebar {}
