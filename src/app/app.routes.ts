e76bfa619815424a311508e468b019fa21bc70d1
devmork <enoymarkden@gmail.com>
2025-09-14 15:25:52 +0800

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Projects } from './pages/projects/projects';
+import { Contact } from './pages/contact/contact';
+import { About } from './pages/about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'projects',
+    component: Projects,
+  },
+  {
+    path: 'about',
+    component: About,
+  },
+  {
+    path: 'contact',
+    component: Contact,
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Sidebar } from './shared/components/sidebar/sidebar';
+import { Main } from './shared/components/main/main';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Sidebar, Main],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('portfolio');
