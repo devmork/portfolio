a125d12eef0dab5c9eb8d0a54ecab8f2beba1b2a
devmork <enoymarkden@gmail.com>
2025-09-14 15:03:29 +0800

    refactor sidebar ui design

--- src/app/shared/components/sidebar/sidebar.ts
+++ src/app/shared/components/sidebar/sidebar.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { Avatar } from '../avatar/avatar';
+import { Footer } from '../footer/footer';
 
 @Component({
-  selector: 'sidebar-component',
-  imports: [],
+  selector: 'app-sidebar',
+  imports: [RouterLink, Avatar, RouterModule, Footer],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
-export class Sidebar {}
+export class Sidebar {
+  pages = [
+    {
+      routeLink: '',
+      icon: 'ri-home-3-line',
+      label: 'Home',
+    },
+    {
+      routeLink: 'projects',
+      icon: 'ri-folder-2-line',
+      label: 'Projects',
+    },
+    {
+      routeLink: 'about',
+      icon: 'ri-user-5-line',
+      label: 'About',
+    },
+    {
+      routeLink: 'contact',
+      icon: 'ri-contacts-book-line',
+      label: 'Contact',
+    },
+  ];
+}
