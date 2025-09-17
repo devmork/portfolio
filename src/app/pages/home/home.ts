84719f51b7001c5b8bacc55eedaa12f8e77f999b
devmork <enoymarkden@gmail.com>
2025-09-17 14:27:10 +0800

    refactor: clean up home component by removing unused code and integrating Technologies component

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,27 +1,10 @@
 import { Component } from '@angular/core';
+import { Technologies } from '../../shared/components/technologies/technologies';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Technologies],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
-  technologies = [
-    { name: 'Angular', png: '/images/angular.png' },
-    { name: '.NET Core', png: '/images/aspnetcore.png' },
-    { name: 'C#', png: '/images/csharp.png' },
-    { name: 'CSS', png: '/images/css.png' },
-    { name: 'Figma', png: '/images/figma.png' },
-    { name: 'Git', png: '/images/git.png' },
-    { name: 'Github', png: '/images/github.png' },
-    { name: 'HTML5', png: '/images/html.png' },
-    { name: 'Javascript', png: '/images/javascript.png' },
-    { name: 'SQLite', png: '/images/sqlite.png' },
-    { name: 'SQL Server', png: '/images/sqlserver.png' },
-    { name: 'Tailwind', png: '/images/tailwind.png' },
-    { name: 'Typescript', png: '/images/typescript.png' },
-    { name: 'Visual Studio', png: '/images/visualstudio.png' },
-    { name: 'VSCode', png: '/images/vscode.png' },
-  ];
-}
+export class Home {}
