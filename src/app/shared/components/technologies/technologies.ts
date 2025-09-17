fa29eb7a6c0fb9efc98f69c317a3907f940cbf98
devmork <enoymarkden@gmail.com>
2025-09-17 14:26:43 +0800

    feat: add Technologies component with dynamic technology list

---
+++ src/app/shared/components/technologies/technologies.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-technologies',
+  imports: [],
+  templateUrl: './technologies.html',
+  styleUrl: './technologies.css',
+})
+export class Technologies {
+  technologies = [
+    { name: 'Angular', png: '/images/angular.png' },
+    { name: '.NET Core', png: '/images/aspnetcore.png' },
+    { name: 'C#', png: '/images/csharp.png' },
+    { name: 'CSS', png: '/images/css.png' },
+    { name: 'Figma', png: '/images/figma.png' },
+    { name: 'Git', png: '/images/git.png' },
+    { name: 'Github', png: '/images/github.png' },
+    { name: 'HTML5', png: '/images/html.png' },
+    { name: 'Javascript', png: '/images/javascript.png' },
+    { name: 'SQLite', png: '/images/sqlite.png' },
+    { name: 'SQL Server', png: '/images/sqlserver.png' },
+    { name: 'Tailwind', png: '/images/tailwind.png' },
+    { name: 'Typescript', png: '/images/typescript.png' },
+    { name: 'Visual Studio', png: '/images/visualstudio.png' },
+    { name: 'VSCode', png: '/images/vscode.png' },
+  ];
+}
