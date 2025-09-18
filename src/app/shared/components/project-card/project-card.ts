7d39582f4387e0886cb1e6710182d132d91a92aa
devmork <enoymarkden@gmail.com>
2025-09-18 21:47:07 +0800

    feat: add project card component with initial project data and styling

---
+++ src/app/shared/components/project-card/project-card.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-card',
+  imports: [],
+  templateUrl: './project-card.html',
+  styleUrl: './project-card.css',
+})
+export class ProjectCard {
+  projects = [
+    {
+      img: 'https://github.com/devmork/product-preview-card/blob/main/design/solution-screenshot.png?raw=true',
+      name: 'Product Preview Card',
+      description:
+        'A Frontend Mentor challenge where I built a product card for a perfume ad. It helped me improve my JavaScript and CSS skills while having fun with the design.',
+      technologies: ['html', 'css'],
+      repository: 'https://github.com/devmork/product-preview-card',
+      website: '',
+    },
+  ];
+}
