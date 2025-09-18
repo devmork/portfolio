25606e62413bb6396aa98608db398b6a6856c216
devmork <enoymarkden@gmail.com>
2025-09-18 21:47:23 +0800

    refactor: simplify projects component by removing hardcoded project data and integrating project card component

--- src/app/pages/projects/projects.ts
+++ src/app/pages/projects/projects.ts
@@ -1,30 +1,10 @@
 import { Component } from '@angular/core';
+import { ProjectCard } from '../../shared/components/project-card/project-card';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [ProjectCard],
   templateUrl: './projects.html',
   styleUrl: './projects.css',
 })
-export class Projects {
-  projects = [
-    {
-      img: 'https://github.com/devmork/product-preview-card/blob/main/design/solution-screenshot.png?raw=true',
-      name: 'Product Preview Card',
-      description:
-        'A Frontend Mentor challenge where I built a product card for a perfume ad. It helped me improve my JavaScript and CSS skills while having fun with the design.',
-      technologies: ['html', 'css'],
-      repository: 'https://github.com/devmork/product-preview-card',
-      website: '',
-    },
-    {
-      img: '',
-      name: 'Contact Card List',
-      description:
-        'My first Angular project from Angular PH Bootcamp—a contact card list. It helped me learn how to use components and display data in a clean, simple way.',
-      technologies: ['html', 'daisyui', 'angular', 'script'],
-      repository: 'https://github.com/devmork/contact-card-list',
-      website: '',
-    },
-  ];
-}
+export class Projects {}
