0a48306d50febf23eedf6851d0f4187d2e0fba54
devmork <enoymarkden@gmail.com>
2025-09-19 16:06:18 +0800

    add educations component

---
+++ src/app/shared/components/educations/educations.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-educations',
+  imports: [],
+  templateUrl: './educations.html',
+  styleUrl: './educations.css',
+})
+export class Educations {
+  educations = [
+    {
+      school: 'Andres Bonifatio College',
+      level: 'Senior High School',
+      course: 'Humanities and Social Sciences',
+      graduated: '2021 - 2023',
+      address: 'Dipolog City, Philippines',
+      img: '/images/abc-logo.jpg',
+    },
+    {
+      school: 'DMCCollege Foundation Inc.',
+      level: 'Bachelors Degree',
+      course: 'Information Technology',
+      graduated: '2023 - 2028',
+      address: 'Dipolog City, Philippines',
+      img: '/images/dmc-logo.png',
+    },
+  ];
+}
